ca24e906083b33ff8530d34986871eb27d0b9e05
Sami Irshad <samiirshad1103@gmail.com>
2023-03-14 22:59:01 +0500

    All Screen Connected

--- CafeConnect/src/main/java/com/example/cafeconnect/HelloApplication.java
+++ CafeConnect/src/main/java/com/example/cafeconnect/HelloApplication.java
@@ -2,38 +2,85 @@ package com.example.cafeconnect;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class HelloApplication extends Application {
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Main Administrator"+".fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
     }
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println(event);
-
-           try {
-               FXMLLoader loader = new FXMLLoader(getClass().getResource("Addsale.fxml"));
-               Stage stage = new Stage();
-               Scene scene = new Scene(loader.load());
-               stage.setScene(scene);
-           }catch (IOException io){
-               io.printStackTrace();
-           }
-
+//    @FXML
+    void Display(ActionEvent event,String name,String Title) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource(name+".fxml"));
+        Scene scene = new Scene(root, 600, 400);
+        Stage Window =(Stage) ((Node)event.getSource()).getScene().getWindow();
+        Window.setTitle(Title);
+        Window.setScene(scene);
+        Window.show();
     }
-    @FXML
+    private String getId(ActionEvent event)
+    {
 
+        String [] id= (event.getSource().toString()).split("=");
+        id=id[1].split(",");
+        return id[0];
+    }
+    public void handleButtonAction(ActionEvent event) throws IOException {
+        System.out.println(event.getSource());
+        String Name="hello-view";
+        String Title="";
+
+        if(getId(event).equals("AddItem"))
+        {
+            Name= "Addsale";
+            Title= "Sale Process";
+        }
+        else if(getId(event).equals("ReturnItem"))
+        {
+            Name= "Return";
+            Title= "Return";
+        }
+        else if(getId(event).equals("Update"))
+        {
+            Name= "Update";
+            Title= "Update";
+        }
+        else if(getId(event).equals("AddCashier"))
+        {
+            Name= "Manage Manager";
+            Title= "Manage Cashier";
+        }
+        else if(getId(event).equals("BackCashier"))
+        {
+            Name= "hello-view";
+            Title= "Cashier";
+        }
+        else if(getId(event).equals("BackMainAdministrator"))
+        {
+            Name= "Main Administrator";
+            Title= "Administrator";
+        }
+        else if(getId(event).equals("BackManageManager"))
+        {
+            Name= "Manage Manager";
+            Title= "Cashier";
+        }
+        Display(event,Name,Title);
+    }
     public static void main(String[] args) {
         launch();
     }
--- CafeConnect/src/main/java/com/example/cafeconnect/HelloController.java
+++ CafeConnect/src/main/java/com/example/cafeconnect/HelloController.java
@@ -5,10 +5,16 @@ import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController  {
+
     @FXML
     private Label welcomeText;
 
